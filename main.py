d2e2105aaa477d95863094d5b15a8b68df9070f3
jainal09 <jainal09gosalia@gmail.com>
2020-10-30 09:20:50 +0530

    updated to latest code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from db_models.mongo_setup import global_init
 from db_models.models.cache_model import Cache
 import uuid
@@ -7,28 +6,42 @@ import init
 from image_recog_service import predict
 import pyfiglet
 import requests
+from init import err_logger
+
 
 global_init()
 
+FILE_ID = ""
+
+
 def save_to_db(db_object, labels, scores):
-    print("in save")
-    print(db_object)
-    print(db_object.id)
-    db_object.labels = labels
-    db_object.scores = scores
-    db_object.save()
+    try:
+        print("*****************SAVING TO DB******************************")
+        print("in save")
+        print(db_object)
+        print(db_object.id)
+        db_object.labels = labels
+        db_object.scores = scores
+        db_object.save()
+        print("*****************SAVED TO DB******************************")
+    except Exception as e:
+        print(" ERROR IN SAVE TO DB")
+        err_logger(str(e)+" ERROR IN SAVE TO DB")
 
-def update_state(file):
+
+def update_state(file_name):
     payload = {
-        'topic_name': globals.RECEIVE_TOPIC,
-        'client_id': globals.CLIENT_ID,
-        'value': file
+        'parent_name': globals.PARENT_NAME,
+        'group_name': globals.GROUP_NAME,
+        'container_name': globals.RECEIVE_TOPIC,
+        'file_name': file_name,
+        'client_id': globals.CLIENT_ID
     }
-
     try:
         requests.request("POST", globals.DASHBOARD_URL,  data=payload)
-    except:
-        print("EXCEPTION IN UPDATE STATE API CALL......")
+    except Exception as e:
+        print(f"{e} EXCEPTION IN UPDATE STATE API CALL......")
+        err_logger(f"{e} EXCEPTION IN UPDATE STATE API CALL......FILE ID {FILE_ID}")
 
 
 if __name__ == '__main__':
@@ -41,16 +54,19 @@ if __name__ == '__main__':
         message = message.value
         db_key = str(message)
         print(db_key, 'db_key')
+        FILE_ID = db_key
         try:
             db_object = Cache.objects.get(pk=db_key)
-        except:
-            print("EXCEPTION IN GET PK... continue")
+            file_name = db_object.file_name
+            print("#############################################")
+            print("########## PROCESSING FILE " + file_name)
+            print("#############################################")
+            final_labels = db_object.labels
+            final_scores = db_object.scores
+        except Exception as e:
+            print("EXCEPTION IN FETCHING FROM DATABASE......")
+            err_logger(str(e) + " EXCEPTION IN FETCHING FROM DATABASE......FILE ID {FILE_ID}")
             continue
-        file_name = db_object.file_name
-        # init.redis_obj.set(globals.RECEIVE_TOPIC, file_name)
-        print("#############################################")
-        print("########## PROCESSING FILE " + file_name)
-        print("#############################################")
         if db_object.is_doc_type:
             """document"""
             if db_object.contains_images:
@@ -60,40 +76,43 @@ if __name__ == '__main__':
                     with open(pdf_image, 'wb') as file_to_save:
                         file_to_save.write(image.file.read())
                     images_array.append(pdf_image)
-                to_save  = []
-                final_labels=db_object.labels
-                final_scores=db_object.scores
                 for image in images_array:
-
                     try:
                         response = predict(file_name=image)
-                    except:
-                        print("ERROR IN PREDICT")
+                        for label, score in zip(response["labels"], response['scores']):
+                            if label not in final_labels:
+                                final_labels.append(label.strip())
+                                final_scores.append(score)
+                            else:
+                                x = final_labels.index(label)
+                                score_to_check = final_scores[x]
+                                if score > score_to_check:
+                                    final_scores[x] = score
+                        save_to_db(db_object, final_labels, final_scores)
+                        print(".....................FINISHED PROCESSING FILE.....................")
+                        update_state(file_name)
+                    except Exception as e:
+                        print(str(e) + "Exception in predict")
+                        err_logger(str(e) + "Exception in predict")
                         continue
-                    # final_labels.extend(response["labels"])
-                    for label,score in zip(response["labels"],response['scores']):
-                        if label not in final_labels:
-                            final_labels.append(label.strip())
-                            final_scores.append(score)
-                        else:
-                            x = final_labels.index(label)
-                            score_to_check = final_scores[x]
-                            if score > score_to_check:
-                                final_scores[x] = score
-
-
-                save_to_db(db_object,final_labels,final_scores)
-                print(".....................FINISHED PROCESSING FILE.....................")
-                update_state(file_name)
-
         else:
             """image"""
-
             with open(file_name, 'wb') as file_to_save:
                 file_to_save.write(db_object.file.read())
-            image_result = predict(file_name)
-            labels=image_result['labels']
-            scores=image_result['scores']
-            save_to_db(db_object,labels,scores)
-            print(".....................FINISHED PROCESSING FILE.....................")
-            update_state(file_name)
+            try:
+                image_results = predict(file_name)
+                for label, score in zip(image_results["labels"], image_results['scores']):
+                    if label not in final_labels:
+                        final_labels.append(label.strip())
+                        final_scores.append(score)
+                    else:
+                        x = final_labels.index(label)
+                        score_to_check = final_scores[x]
+                        if score > score_to_check:
+                            final_scores[x] = score
+                save_to_db(db_object, final_labels, final_scores)
+                print(".....................FINISHED PROCESSING FILE.....................")
+                update_state(file_name)
+            except Exception as e:
+                print(str(e) + " Exception in predict")
+                err_logger(str(e) + " Exception in predict")
